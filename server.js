99baa5948e26318f4856b6a4c4ef02ff69a3b614
unknown <krishbansalbrother@gmail.com>
2025-04-16 21:31:23 +0530

    Updated README and fixed typo in script

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/upload', uploadRoute);
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
